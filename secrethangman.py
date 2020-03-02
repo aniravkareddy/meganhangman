fff3a250c74596924d2749e9c2afb7aca3403d5d
aniravkareddy <60886354+aniravkareddy@users.noreply.github.com>
2020-03-02 10:08:09 -0500

    Add files via upload

--- secrethangman.py
+++ secrethangman.py
@@ -3,32 +3,54 @@ randomWords = ["ducks" , "jumbo" ,"lucky" , "pills" , "flour"]
 secret = random.choice (randomWords)
 letter = ""
 updateWord = []
-#print secret 
+print secret 
 def initialize(): 
-    print "We have a secret word"
+    print secret
+    print ("We have a secret word")
     print "_ _ _ _ _"
 def getLetter():
     print "Enter a letter"
     global letter
-    letter = raw_input()
+    letter = str(raw_input())
 def ifWon():
-    if secret == updateWord: 
-        print "you win"
+    if secret == updateWord:
+        i = 0
+        while i<len(secret):
+            if secret[i] == letter:
+                pos=secret.find(letter)
+                updateWord[pos] = letter
+        print updateWord.join()
+        print ("you win")
+        
     else:
         getLetter()
 def main():
-    initialize ()
-    getLetter()
-    ifWon()
+    while True:
+        initialize ()
+        getLetter()
+        ifWon()
 main()
+def test():
+    global secret
+    print secret
+    global updateWord
+    letter=int(input("What is letter :"))
+    if letter in secret:
+        pos=secret.find(letter)
+        updateWord[pos] = letter
+        print updateWord.join()
+        print ("yay")
+        ifWon()
+    else:
+        getLetter()
+        print("no")
+        
 
-#def tester():
-    #global updateWord
-    #updateWord = raw_input()
-    #if updateWord== secret:
-     #   print "yay"
-    #else: 
-     #   print "no"
+
+
+
+
+
   
     
     
